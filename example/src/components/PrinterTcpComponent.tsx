b221cc1f41ec47dbd203bee52c6d22e9e5129cad
Lenny4 <alexandre.beaujour@hotmail.fr>
2022-02-01 19:41:34 -0400

    feat: print tcp works in example

---
+++ example/src/components/PrinterTcpComponent.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Button, Text, TextInput, View } from 'react-native';
+import { Style } from '../core/Style';
+import ThermalPrinter from 'react-native-thermal-printer';
+
+const PrinterTcpComponent: React.FC = React.memo(() => {
+  const [ip, setIp] = React.useState<string>('192.168.1.42');
+  const [port, setPort] = React.useState<string>('9100');
+  const [result, setResult] = React.useState<string>('');
+
+  const testPrint = React.useCallback(async () => {
+    try {
+      await ThermalPrinter.printTcp({
+        ip: ip,
+        port: parseInt(port, 10),
+        payload:
+          "[C]<u><font size='big'>" +
+          'Test success !' +
+          '</font></u>\n' +
+          '[L] \n' +
+          '[L] \n',
+      });
+      setResult('Success !');
+    } catch (e) {
+      setResult('Failed !');
+    }
+  }, [ip, port]);
+
+  return (
+    <View>
+      <Text style={Style.h1}>PrinterTcpComponent</Text>
+      <TextInput
+        style={[Style.input, Style.mt15]}
+        onChangeText={setIp}
+        value={ip}
+        placeholder="IP of the printer"
+        keyboardType="numeric"
+      />
+      <TextInput
+        style={[Style.input, Style.mt15, Style.mb15]}
+        onChangeText={setPort}
+        value={port}
+        placeholder="Port of the printer"
+        keyboardType="numeric"
+      />
+      <Button onPress={testPrint} title="Test print" />
+      <Text style={Style.mt15}>Result: {result}</Text>
+    </View>
+  );
+});
+export default PrinterTcpComponent;
